8e1c5b3dc3a144f6bbf1d2df72a77144e302535f
Vera Sanches <vera.sanches@sorocaba.sp.gov.br>
2023-08-28 14:37:09 -0300

    Adicionando console LOG

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("O Paulo fez esse commit sem vc ver na aula")
+console.log("Enviando para o GIT HUB")
