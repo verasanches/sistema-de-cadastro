7597b02ea9a9f4b43bdca2f10ddd0aadc3d4f7b3
Vera Sanches <vera.sanches@sorocaba.sp.gov.br>
2023-08-28 14:52:16 -0300

    voltando para o estado do paulo

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("O Paulo fez esse commit sem vc ver na aula")
-console.log("Enviando para o GIT HUB")
